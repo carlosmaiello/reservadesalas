b309a47c3079813247a465401a4ecf2de3722106
CARLOS ALBERTO MAIELLO JUNIOR <carlos.amjunior2@senacsp.edu.br>
2022-03-14 21:59:04 -0300

    Sala

--- src/components/MapaAlocacao.vue
+++ src/components/MapaAlocacao.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="mapa-alocacao">
-    <div class="mapa-alocacao-topo"></div>
+    <div class="mapa-alocacao-topo">
+      <MapaAlocacaoDatas :inicio="inicio" :fim="fim" />
+    </div>
     <div class="mapa-alocacao-salas">
       <MapaAlocacaoSala :sala="sala" v-for="sala in salas" :key="sala.id" :inicio="inicio" :fim="fim" />
     </div>
@@ -9,9 +11,10 @@
 
 <script>
 import MapaAlocacaoSala from "./MapaAlocacaoSala.vue";
+import MapaAlocacaoDatas from "./MapaAlocacaoDatas.vue";
 export default {
   props: ["predio", "periodo", "salas"],
-  components: { MapaAlocacaoSala },
+  components: { MapaAlocacaoSala, MapaAlocacaoDatas },
   data() {
     return {
       inicio: new Date(2022, 2, 1),
@@ -22,8 +25,10 @@ export default {
 </script>
 
 <style>
+.mapa-alocacao { overflow-x: auto; }
 .mapa-alocacao-sala { display: flex; }
-.mapa-alocacao-sala-nome { width: 200px; }
-.mapa-alocacao-sala-dia { width: 30px; }
-.mapa-alocacao-reserva { display: block; width: 20px; height: 20px; background: red; border-radius: 50%; font-size: 0!important;}
+.mapa-alocacao-sala-nome { width: 200px; flex: 0 0 200px; }
+.mapa-alocacao-topo { padding-left: 200px; display: flex; }
+.mapa-alocacao-sala-dia { width: 40px; text-align: center; flex: 0 0 40px;}
+.mapa-alocacao-reserva { display: inline-block; width: 20px; height: 20px; background: red; border-radius: 50%; font-size: 0!important;}
 </style>
--- src/components/MapaAlocacaoDatas.vue
+++ src/components/MapaAlocacaoDatas.vue
@@ -1,10 +1,29 @@
 <template>
-  
+    <div class="mapa-alocacao-sala-dia" v-for="dia in dias" :key="dia">
+      {{ dia.getDate() }}<br>
+      {{ diaSemana(dia) }}
+    </div>
 </template>
 
 <script>
+const weekday = ["D","S","T","Q","Q","S","S"];
 export default {
+props: ["inicio", "fim"],
+  computed: {
+    dias() {
+      let dias = [];
+      for (let d = new Date(Number(this.inicio)); d <= this.fim; d.setDate(d.getDate() + 1)) {
+        dias.push(new Date(Number(d)));
+      }
 
+      return dias;
+    },
+  },
+  methods: {
+      diaSemana(dia) {
+          return weekday[dia.getDay()];
+      }
+  }
 }
 </script>
 
